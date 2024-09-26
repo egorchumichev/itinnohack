c41f9203019fe737523fe39bba58fa27f98db03c
Lextonian <chubektumoxa@gmail.com>
2024-09-26 19:15:41 +0500

    error handling in tasks

--- app/api/tasks/route.js
+++ app/api/tasks/route.js
@@ -1,7 +1,35 @@
 import prisma from '../../../prisma/client';
 
+const enumirate = [
+    "PENDING",
+    "IN_PROGRESS",
+    "COMPLETED"
+]
+
 export async function POST(request) {
     const { title, status, projectId, userId } = await request.json();
+
+    const existingProject = await prisma.project.findUnique({
+        where: { id: projectId},
+    });
+
+    const existingUser = await prisma.user.findUnique({
+        where: {id :userId },
+    });
+
+    if (status) if (!enumirate.includes(status)) {
+        return new Response(JSON.stringify({ massage: 'Неподдерживаемый статус'}), { status: 400 });
+    }
+
+    if (!existingProject) {
+        return new Response(JSON.stringify({ error: `Проекта с id ${projectId} нет` }), { status: 400 });
+    }
+
+    if (!existingUser) {
+        return new Response(JSON.stringify({ error: `Пользователя c id ${userId} нет` }), { status: 400 });
+    }
+
+    
 
     const task = await prisma.task.create({
         data: { title, status, projectId, userId },
@@ -14,11 +42,21 @@ export async function GET(request) {
     const { searchParams } = new URL(request.url);
     const status = searchParams.get('status');
     const projectId = searchParams.get('projectId');
-    
+
     const where = {};
+    
+    if (status) if (!enumirate.includes(status)) {
+        return new Response(JSON.stringify({ massage: 'Неподдерживаемый статус'}), { status: 400 });
+    }
+    
     if (status) where.status = status;
     if (projectId) where.projectId = Number(projectId);
 
     const tasks = await prisma.task.findMany({ where });
+
+    if (!tasks.length) {
+        return new Response(JSON.stringify({ massage: 'С заданными фильтрами задач нет'}), { status: 200 });
+    }
+
     return new Response(JSON.stringify(tasks), { status: 200 });
 }
